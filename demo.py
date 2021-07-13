beb1906acf5585e8d742cb1193f40c47fb971416
Dayana Alejandro <dayana200120@gmail.com>
2021-07-13 16:39:12 +0000

    Uses speech recognition

--- demo.py
+++ demo.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm
 from flask_sqlalchemy import SQLAlchemy
+from converter import printWAV # get speech recognition function
+import time, random, threading
+from turbo_flask import Turbo # pip3 install turbo-flask
 
 
 app = Flask(__name__)                    # this gets the name of the file so Flask knows it's name
 app.config['SECRET_KEY'] = '67414c2f94271f30852f5623dbeb57b3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+interval=10
+FILE_NAME = "english.wav"
+turbo = Turbo(app)
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -38,12 +44,47 @@ def register():
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
-  
+
+
 @app.route("/captions")
 def captions():
     TITLE = "The Danger Of Silence"
-    FILE_NAME = "The danger of silence _ Clint Smith.wav"
-    return render_template('captions.html', songName=TITLE, file=FILE_NAME)
+    return render_template('captions.html', songName=TITLE, file=FILE_NAME)  
+
+@app.before_first_request
+def before_first_request():
+    #resetting time stamp file to 0
+    file = open("pos.txt","w") 
+    file.write(str(0))
+    file.close()
+
+    #starting thread that will time updates
+    threading.Thread(target=update_captions).start()
+
+@app.context_processor
+def inject_load():
+    # getting previous time stamp
+    file = open("pos.txt","r")
+    pos = int(file.read())
+    file.close()
+
+    # writing next time stamp
+    file = open("pos.txt","w")
+    file.write(str(pos+interval))
+    file.close()
+
+    #returning captions
+    return {'caption':printWAV(FILE_NAME, pos=pos, clip=interval)}
+
+def update_captions():
+    with app.app_context():
+        while True:
+            # timing thread waiting for the interval
+            time.sleep(interval)
+
+            # forcefully updating captionsPane with caption
+            turbo.push(turbo.replace(render_template('captionsPane.html'), 'load'))
+
 
 '''
 To View Users: run python3
