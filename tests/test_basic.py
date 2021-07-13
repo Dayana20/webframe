9e2a38c216b1a8d233d2b89a7868a972a3ec10d1
Dayana Alejandro <dayana200120@gmail.com>
2021-07-13 18:00:36 +0000

    Undo

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -13,25 +13,25 @@ class BasicTests(unittest.TestCase):
     #### tests ####
     ###############
 
-#     def test_main_page(self):
-#       print("Here")
-#       response = self.app.get('/', follow_redirects=True)
-#       self.assertEqual(response.status_code, 200)
+    def test_main_page(self):
+      print("Here")
+      response = self.app.get('/', follow_redirects=True)
+      self.assertEqual(response.status_code, 200)
       
-#     def test_about_page(self):
-#         print("Here2")
-#         response = self.app.get('/about', follow_redirects=True)
-#         self.assertEqual(response.status_code, 200)
+    def test_about_page(self):
+        print("Here2")
+        response = self.app.get('/about', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
         
-#     def test_second_page(self):
-#         print("Here3")
-#         response = self.app.get('/second_page', follow_redirects=True)
-#         self.assertEqual(response.status_code, 200)
+    def test_second_page(self):
+        print("Here3")
+        response = self.app.get('/second_page', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
         
-#     def test_register(self):
-#         print("Here4")
-#         response = self.app.get('/register', follow_redirects=True)
-#         self.assertEqual(response.status_code, 200)
+    def test_register(self):
+        print("Here4")
+        response = self.app.get('/register', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
     unittest.main(exit=False)
